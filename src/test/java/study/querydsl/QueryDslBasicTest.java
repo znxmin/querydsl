34df672b946823a241196207aa2657f32d8ab93b
limminjeong <limminjeong98@naver.com>
2023-05-21 17:29:26 +0900

    정렬

--- src/test/java/study/querydsl/QueryDslBasicTest.java
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -148,4 +148,28 @@ public class QueryDslBasicTest {
         //         .selectFrom(member)
         //         .fetchCount();
     }
+
+    @Test
+    void sort() {
+        // 회원 정렬 순서
+        // 1. 나이 내림차순
+        // 2. 이름 오름차순
+        // 이름이 없으면 마지막에 출력 (nulls last)
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+        Assertions.assertThat(member5.getUsername()).isEqualTo("member5");
+        Assertions.assertThat(member6.getUsername()).isEqualTo("member6");
+        Assertions.assertThat(memberNull.getUsername()).isNull();
+    }
 }
